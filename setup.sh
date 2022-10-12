a70c9ce88233eb50ae3f8b253c444a3495979504
Pranay Umredkar <71922973+PranayUmredkar97@users.noreply.github.com>
2022-10-12 17:27:13 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,22 @@
-mkdir -p ~/.streamlit/
+# mkdir -p ~/.streamlit/
 
+# echo "\
+# [server]\n\
+# port = $PORT\n\
+# enableCORS = false\n\
+# headless = true\n\
+# \n\\
+# " > ~/.streamlit/config.toml
+
+
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"pranuumredkar970@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
-port = $PORT\n\
-enableCORS = false\n\
 headless = true\n\
-\n\\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
